4e4cb7405786b43bfb1ddc46e5f038a65cca8a90
Muzaffar.Yuldashev <yuldashevmrb@gmail.com>
2022-05-12 05:04:14 +0500

    number1

---
+++ src/test/java/mrb/yuldashev/ls/LsApplicationTests.java
@@ -0,0 +1,13 @@
+package mrb.yuldashev.ls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
